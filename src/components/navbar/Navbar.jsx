18eb65a92c7b876ec6bdd5d48c6014de0a4b35e6
KleverKobold <104393766+KleverKobold@users.noreply.github.com>
2023-04-26 22:06:22 -0300

    Possibilities Section Finished

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,16 +8,7 @@ import "./navbar.css"
 import { useState } from "react";
 
 
-//Menu of Links
-const Menu = () =>{
-  <>
-   <p><a href="#home">Home</a></p>
-   <p><a href="#wgpt3">What is GPT</a></p>
-   <p><a href="#possibility">Open AI</a></p>
-   <p><a href="#features">Case Studies</a></p>
-   <p><a href="#blog">Library</a></p>
-  </>
-}
+
 
 
 
@@ -32,9 +23,9 @@ const Navbar = () => {
        </div>
           <div className='gpt3__navbar-links_container'>
           <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT</a></p>
-            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#wgpt3">What is GPT3</a></p>
             <p><a href="#features">Case Studies</a></p>
+            <p><a href="#possibility">Open AI</a></p>
             <p><a href="#blog">Library</a></p>
           </div>
       </div>
